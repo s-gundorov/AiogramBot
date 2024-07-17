5b2adb333a3fc685265c3fdbe5ce3a48c69d18a8
s-gundorov <46057845+s-gundorov@users.noreply.github.com>
2024-07-17 14:11:57 +0300

    Прогноз погоды

---
+++ main.py
@@ -0,0 +1,103 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from gtts import gTTS
+import os
+from googletrans import Translator
+
+from config import TOKEN
+import random
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+translator = Translator()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f'Привет, {message.from_user.first_name}')
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer("Этот бот умеет выполнять команды:\n/start \n/help")
+
+@dp.message(F.text == "что такое ИИ?")
+async def aitext(message: Message):
+    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+
+@dp.message(F.photo)
+async def react_photo(message: Message):
+#    await message.answer('Спасибо за фото!')
+    list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Спасибо за фото!']
+    rand_answ = random.choice(list)
+    await message.answer(rand_answ)
+    await bot.download(message.photo[-1],destination=f'tmp/{message.photo[-1].file_id}.jpg')
+
+@dp.message(Command('photo'))
+async def photo(message: Message):
+    photos = ['https://pisoft.ru/images/disc_ps6.jpg', 'https://pisoft.ru/images/aprok/aprok_cd_sm.png', 'https://pisoft.ru/images/prok/prokat_cd10_sm.png']
+    rand_photo = random.choice(photos)
+#    random_photo = random.choice(photos)
+    await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка')
+
+@dp.message(Command('video'))
+async def video(message: Message):
+    await bot.send_chat_action(message.chat.id, 'upload_video')
+    video = FSInputFile('test.mp4')
+    await bot.send_video(message.chat.id, video)
+
+@dp.message(Command('audio'))
+async def audio(message: Message):
+    audio = FSInputFile('sound.mp3')
+    await bot.send_audio(message.chat.id, audio)
+
+# Озвучка текста:
+@dp.message(Command('training'))
+async def training(message: Message):
+   training_list = [
+       "Тренировка 1:\n1. Скручивания: 3 подхода по 15 повторений\n2. Велосипед: 3 подхода по 20 повторений (каждая сторона)\n3. Планка: 3 подхода по 30 секунд",
+       "Тренировка 2:\n1. Подъемы ног: 3 подхода по 15 повторений\n2. Русский твист: 3 подхода по 20 повторений (каждая сторона)\n3. Планка с поднятой ногой: 3 подхода по 20 секунд (каждая нога)",
+       "Тренировка 3:\n1. Скручивания с поднятыми ногами: 3 подхода по 15 повторений\n2. Горизонтальные ножницы: 3 подхода по 20 повторений\n3. Боковая планка: 3 подхода по 20 секунд (каждая сторона)"
+   ]
+   rand_tr = random.choice(training_list)
+   await message.answer(f"Это ваша мини-тренировка на сегодня\n {rand_tr}")
+
+   tts = gTTS(text=rand_tr, lang='ru')
+   tts.save("training.ogg")
+   audio = FSInputFile('training.ogg')
+   await bot.send_voice(chat_id=message.chat.id, voice=audio)
+   os.remove("training.ogg")
+
+# Озвучка аудио-сообщения:
+@dp.message(Command('voice'))
+async def voice(message: Message):
+    voice = FSInputFile("sample.ogg")
+    await message.answer_voice(voice)
+
+# Отправка документа:
+@dp.message(Command('doc'))
+async def doc(message: Message):
+	doc = FSInputFile("ProkatDat1_1.txt")
+	await bot.send_document(message.chat.id, doc)
+
+# Реакция на ключевую фразу:
+async def start(message: Message):
+    if (message.text.lower() == 'test' or message.text.lower() == 'тест'):
+        await message.answer('Тестируем')
+
+# Эхо:
+#@dp.message()
+#async def start(message: Message):
+#    await message.send_copy(chat_id=message.chat.id)
+
+@dp.message()
+async def translate_message(message: Message):
+    if message.text:
+        translation = translator.translate(message.text, dest='ru')
+        await message.answer(translation.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ new.py
@@ -0,0 +1,95 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+from googletrans import Translator
+import aiohttp
+import logging
+
+from config import TOKEN, WEATHER_API_KEY
+import sqlite3
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+translator = Translator()
+
+
+logging.basicConfig(level=logging.INFO)
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    city = State()
+
+# Создание таблицы users
+def init_db():
+	conn = sqlite3.connect('user_data.db')
+	cur = conn.cursor()
+	cur.execute('''
+	CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	age INTEGER NOT NULL,
+	city TEXT NOT NULL)
+	''')
+	conn.commit()
+	conn.close()
+
+init_db()
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет! Как тебя зовут?")
+    await state.set_state(Form.name)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Из какого ты города?")
+    await state.set_state(Form.city)
+
+@dp.message(Form.city)
+async def city(message: Message, state:FSMContext):
+    await state.update_data(city=message.text)
+    user_data = await state.get_data()
+
+    conn = sqlite3.connect('user_data.db')
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO users (name, age, city) VALUES (?, ?, ?)''', (user_data['name'], user_data['age'], user_data['city']))
+    conn.commit()
+    conn.close()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"http://api.openweathermap.org/data/2.5/weather?q={user_data['city']}&appid={WEATHER_API_KEY}&units=metric") as response:
+            if response.status == 200:
+                weather_data = await response.json()
+                main = weather_data['main']
+                weather = weather_data['weather'][0]
+                temperature = main['temp']
+                humidity = main['humidity']
+                description = weather['description']
+                translation = translator.translate(description, dest='ru')
+                weather_report = (f"Город: {user_data['city']}\n"
+                                  f"Температура: {temperature} гр.\n"
+                                  f"Влажность: {humidity} %\n"
+                                  f"Описание: {translation.text}")
+                await message.answer(weather_report)
+            else:
+                await message.answer("Не удалось получить данные о погоде")
+    await state.clear()
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
